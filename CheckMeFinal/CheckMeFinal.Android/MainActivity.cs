c9cb3a026faaca822251816bcd04271a2c66b73b
Harugot <33010386+Harugot@users.noreply.github.com>
2019-03-25 18:29:21 -0500

    CheckMe

---
+++ CheckMeFinal/CheckMeFinal.Android/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Plugin.Media;
+using Android.Support.V4.App;
+using Android;
+using System.IO;
+using Android.Content;
+
+namespace CheckMeFinal.Droid
+{
+    [Activity(Label = "CheckMeFinal", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            await CrossMedia.Current.Initialize();
+
+            string fileName = "users_db.sqlite";
+            string fileLocation = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string full_path = Path.Combine(fileLocation, fileName);
+
+            //string defaultPath = String.Format(Path.Combine((string)Android.OS.Environment.ExternalStorageDirectory, "CheckMe"));
+
+            string defaultPath = String.Format(fileLocation);
+
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App(full_path, defaultPath));
+
+            ActivityCompat.RequestPermissions(this, new string[] { Manifest.Permission.Camera, Manifest.Permission.ReadExternalStorage, Manifest.Permission.WriteExternalStorage }, 1);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ CheckMeFinal/CheckMeFinal.iOS/Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using Plugin.Media;
+using UIKit;
+
+namespace CheckMeFinal.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            Application app = new Application();
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+
+            app.Initialize();
+        }
+
+        public async void Initialize()
+        {
+            await CrossMedia.Current.Initialize();
+        }
+    }
+}
